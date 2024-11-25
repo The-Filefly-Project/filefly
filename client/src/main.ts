d4c20838e87783ab98f3613fa93f8763b8ba969c
4Sight <116753476+4S1ght@users.noreply.github.com>
2024-11-25 21:05:44 +0100

    prep server & client

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.scss'
-import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import './style.scss'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
