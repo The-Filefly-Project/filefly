a02ae22d85435f410b40faa1ad5f840c2dc0c29e
4Sight <116753476+4S1ght@users.noreply.github.com>
2024-11-26 23:58:37 +0100

    basic component setup (will be redone)

---
+++ client/src/components/container/Card.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script> 
+
+<template class="login">
+    <div class="card">
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+
+    .card {
+        border: solid 1px var(--bdc-primary);
+        border-radius: var(--bdr-2);
+        padding: var(--gap-2);
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+    }
+
+</style>
--- client/src/components/login/LoginScreen.vue
+++ client/src/components/login/LoginScreen.vue
@@ -1,11 +1,51 @@
 <script setup lang="ts">
 
+    import Card from '../container/Card.vue'
+    import TextInput from '../input/TextInput.vue'
+
 </script>
 
-<template>
-    login
+<template class="login">
+    <div class="login-screen">
+
+        <div class="prompt">
+            <Card>
+                <form>
+
+                    <label for="username">Username</label>
+                    <TextInput name="username"/>
+
+                    <label for="password">Password</label>
+                    <TextInput name="password"/>
+
+                </form>
+            </Card>
+        </div>
+
+    </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+
+    .login-screen {
+        position: fixed;
+        height: 100vh;
+        width: 100vw;
+        top: 0;
+        left: 0;           
+    }
+
+    .prompt {
+        max-width: 320px;
+        max-height: 450px;
+        position: relative;
+        left: 50%;
+        transform: translateX(-50%);
+        padding-top: 5rem;
+
+        label {
+            display: block;
+        }
+    }
 
 </style>
